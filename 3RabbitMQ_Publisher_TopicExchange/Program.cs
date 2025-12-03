6cdd478e5ab595da5fe28d39d2e201fc8f17ca61
omeraydin41 <omerenver1317@gmail.com>
2025-12-03 20:23:47 +0300

    Learned how to use RabbitMQ Topic Exchange and save error logs to a file
    Tried dynamic routing keys so I can filter logs easily

---
+++ 3RabbitMQ_Publisher_TopicExchange/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    public enum LogsName
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://djufkfnr:Ljr4OZWoMz8PemaiC35bJjXUjN6WYXRD@moose.rmq.cloudamqp.com/djufkfnr");
+
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.ExchangeDeclareAsync(
+            "logs-topic",
+            type: ExchangeType.Topic,//exchange tipini değiştirdik 
+            durable: true
+        );
+
+
+
+        var list = Enumerable.Range(1, 20).ToList();
+
+        foreach (var item in list)
+        {
+
+            Random rnd = new Random();
+            LogsName log1 = (LogsName)rnd.Next(1, 5);
+            LogsName log2 = (LogsName)rnd.Next(1, 5);
+            LogsName log3 = (LogsName)rnd.Next(1, 5);
+
+            var rotKey = $"{log1}.{log2}.{log3}";
+
+            var message = $"{log1}-{log2}-{log3} log!";
+            var bodyMessage = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(
+                exchange: "logs-topic",
+                routingKey: rotKey,
+                mandatory: false,
+                body: bodyMessage
+            );
+
+            Console.WriteLine($"log gönderildi : {message}");
+
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ 3RabbitMQ_Subscriber_TopicExchange/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://djufkfnr:Ljr4OZWoMz8PemaiC35bJjXUjN6WYXRD@moose.rmq.cloudamqp.com/djufkfnr");
+
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 5, global: false);
+
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        var result = await channel.QueueDeclareAsync();
+        var qname = result.QueueName;
+
+        var rootKey = "*.Error.*";
+        await channel.QueueBindAsync(qname, "logs-topic", "*.Error.*");//kuyruk BİND etmek abone duştuğunde kuyruk ta kapanır 
+
+        await channel.BasicConsumeAsync(          // await → işlemi asenkron yürütür
+            qname,                                // string qname → dinlenecek kuyruk adı
+            false,                                // bool autoAck → otomatik onay kapalı
+            consumer                              // AsyncEventingBasicConsumer consumer → mesajları işleyecek tüketici
+        );
+        Console.WriteLine("loglar dinleniyor");
+        consumer.ReceivedAsync += (sender, e) =>        // object sender, BasicDeliverEventArgs e → mesaj geldiğinde tetiklenen event
+        {
+            var message = Encoding.UTF8.GetString(       // string message → gelen mesaj metni
+                e.Body.ToArray()                         // byte[] → mesajın byte dizisine çevrilmesi
+            );
+            Console.WriteLine("Gelen mesaj: " + message); // Konsola mesajı yazdırı
+            File.AppendAllText(                          // Mesajı dosyaya ekler
+                "log-critical.txt",                      // string → yazılacak dosya adı
+                message + "\n"                           // string → yazılacak içerik
+            );
+            channel.BasicAckAsync(                       // Mesajı başarıyla işlendi olarak işaretler
+                e.DeliveryTag,                           // ulong DeliveryTag → mesajın benzersiz etiketi
+                false                                    // bool multiple → tek bir mesajı onayla
+            );
+            return Task.CompletedTask;
+            Console.ReadLine();
+        };
+    }
+}
