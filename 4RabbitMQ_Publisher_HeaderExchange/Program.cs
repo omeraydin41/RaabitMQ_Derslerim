b04e9ba8cb8162cff39555b6653192996bdd1948
omeraydin41 <omerenver1317@gmail.com>
2025-12-04 18:20:40 +0300

    Implement Header Exchange with custom message headers and dynamic queue binding
    - Added Header Exchange structure to publisher and subscriber
    - Included custom headers (format, shape) for message routing

---
+++ 4RabbitMQ_Publisher_HeaderExchange/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    public enum LogsName // Log seviyelerini enum olarak yazdım, daha düzenli olsun diye
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory(); // RabbitMQ bağlantısı kurmak için fabrika nesnesi oluşturuyorum
+        factory.Uri = new Uri("amqps://djufkfnr:Ljr4OZWoMz8PemaiC35bJjXUjN6WYXRD@moose.rmq.cloudamqp.com/djufkfnr"); // CloudAMQP bağlantı adresini verdim
+
+        using var connection = await factory.CreateConnectionAsync(); // Sunucuya async şekilde bağlantı açıyorum
+        using var channel = await connection.CreateChannelAsync(); // Açılan bağlantı üzerinden bir channel oluşturuyorum
+
+        await channel.ExchangeDeclareAsync(
+            "header-exchange",               // Exchange adını yazıyorum
+            type: ExchangeType.Headers,      // Tipi header exchange olarak belirttim
+            durable: true                    // Sunucu kapanınca kaybolmasın diye durable yaptım
+        );
+
+        Dictionary<string, object> headers = new Dictionary<string, object>(); // Header bilgilerini tutacak dictionary oluşturdum
+        headers.Add("format", "pdf"); // Header’a format bilgisini ekledim
+        headers.Add("shape", "a4");   // Header’a sayfa şekli bilgisini ekledim
+
+        var properties = new BasicProperties // Mesajın özelliklerini burada oluşturuyorum
+        {
+            Headers = headers // Oluşturduğum header sözlüğünü buraya veriyorum
+        };
+
+        string message = "This is a header exchange test message."; // Göndermek istediğim mesaj
+        var body = Encoding.UTF8.GetBytes(message); // Mesajı byte dizisine çevirdim çünkü RabbitMQ byte alıyor
+
+        await channel.BasicPublishAsync(
+            exchange: "header-exchange",  // Mesajı göndereceğim exchange
+            routingKey: string.Empty,     // Header exchange routing key kullanmadığı için boş geçiyorum
+            mandatory: false,             // Mesaj zorunlu değil, kuyruk yoksa geri dönmesin diye false yaptım
+            basicProperties: properties,  // Mesajın header ve diğer özellikleri burada
+            body: body                    // Göndereceğim asıl mesaj (byte formatında)
+        );
+
+        Console.WriteLine("log gönderildi."); // Mesajın gittiğini konsola yazdırıyorum
+
+        Console.ReadLine(); // Console kapanmasın diye bekletiyorum
+    }
+
+
+}
+
